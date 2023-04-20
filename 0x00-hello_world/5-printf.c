b9d49ad4a5b80ed9d63641daa27ccf8751a87d57
astrotolu <astrogirl788@gmail.com>
2023-04-20 01:33:01 -0700

    Updated 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
-/*
+/**
  * main - serves as the starting point for program execution.
  *
  * return - serves as the starting point for program execution.
  *
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	retutn (0);
-
+	return (0);
 }
