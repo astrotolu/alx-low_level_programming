31bf17ae9effd138814b9a12355829d4d9093365
astrotolu <astrogirl788@gmail.com>
2023-09-09 16:19:17 -0700

    code will assign a random number to the variable n each time it is executed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,14 +4,14 @@
 
 /**
  * main - starts the code
- * code will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative.
+ * code will assign a random number to the variable n each time it is executed.
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
 
-	srand(time(0);
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
